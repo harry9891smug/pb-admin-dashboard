c05da2b7b0431fbeb7d432d24a87d0fde8a87e3e
harrysmug9891 <harry.smugchucks@gmail.com>
2026-04-04 10:53:48 +0530

    support functionality

---
+++ src/lib/socket.ts
@@ -0,0 +1,9 @@
+import { io, Socket } from "socket.io-client";
+
+const SOCKET_URL =
+  process.env.NEXT_PUBLIC_WS_URL!;
+
+export const socket: Socket = io(SOCKET_URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
